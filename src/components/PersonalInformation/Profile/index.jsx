17c24b1916def6b018d4a48bddea75dd0a9e1c57
Carlos Rafael <carlitos051186@gmail.com>
2020-09-12 00:04:23 -0400

    layout for desktop

--- src/components/PersonalInformation/Profile/index.jsx
+++ src/components/PersonalInformation/Profile/index.jsx
@@ -7,15 +7,15 @@ import ProfileInfo from './ProfileInfo';
 export default function Profile() {
     return (
         <section className="personal-information-profile">
-            <Container fluid>
-                <Row>
+            <Container fluid className="profile-wraper">
+                <Row className="d-md-flex d-none">
                     <Col><h1>Information</h1></Col>
                 </Row>
                 <Row>
                     <Col md={7}>
                         <ProfileForm />
                     </Col>
-                    <Col md={5} xs={{ order: 'first' }}>
+                    <Col md={{ size: 5, order: 'last' }} xs={{ order: 'first' }}>
                         <ProfileInfo />
                     </Col>
                 </Row>
